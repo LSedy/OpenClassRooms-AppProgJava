e7644311940940aa10c5920928f21cd1c3950c96
Sylthen <Sylthen@LAPTOP-U19D5166>
2020-01-11 21:58:47 +0100

    classmain and jre in project

---
+++ src/ClassMain.java
@@ -0,0 +1,12 @@
+
+public class ClassMain {
+
+	public static void main(String[] args) {
+		//Vos données, variables, différents traitements…
+
+	}//Fin de la méthode main
+
+}//Fin de votre classe
+
+
+	    
